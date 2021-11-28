3f0df30190942a52e16727ac99a385f6b434239a
kenji <kenjix12x22@gmail.com>
2021-11-29 00:55:34 +0900

    22.型変換

---
+++ 4basic-type/22.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+//型変換
+
+func main() {
+	fl64 := 10.2
+
+	n := int(fl64)
+	nn := float64(n)
+
+	var s string = "100"
+
+	i, _ := strconv.Atoi(s)
+	fmt.Printf("%T %v\n", i, i)
+
+	h := "Hello World"
+	fmt.Println(string(h[0]))
+
+	var ii int = 321
+	var ss string
+	ss = strconv.Itoa(ii)
+	fmt.Println(ss)
+
+}
